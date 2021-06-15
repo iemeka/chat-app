98e9e1acd6ac4baa7a2f562c407fb83e546b7b7a
iemeka <mekstines@gmail.com>
2021-06-15 12:55:15 +0100

    structuring completed

--- backend/index.js
+++ backend/index.js
@@ -13,17 +13,8 @@ const io = new Server(httpServer, {
   },
 });
 const eventHandlers = require("./event-handler");
-let username = "";
 
-app.use(express.urlencoded({extended:true}))
-
-app.use(express.static("public"));
-
-app.post("/submit-form", (req, res) => {
-  username = req.body.username;
-  // validiat user name
-  res.redirect('http://localhost:3000');
-});
+app.use(express.urlencoded({ extended: true }));
 
 const onConnection = (socket) => {
   eventHandlers(io, socket);
