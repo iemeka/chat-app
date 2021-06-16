67ff313fb765e4a5d0d4eb5f3518564dd0fbf4ce
iemeka <mekstines@gmail.com>
2021-06-16 16:14:42 +0100

    fixed deployment

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 //socket config
 const http = require("http");
 const httpServer = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -15,7 +15,9 @@ const io = new Server(httpServer, {
 const eventHandlers = require("./event-handler");
 
 app.use(express.urlencoded({ extended: true }));
-
+app.get('/', function (req, res) {
+  res.send("<h1>Watch this space. Real time app coming up. By the way, Hello World!</h1>")
+})
 const onConnection = (socket) => {
   eventHandlers(io, socket);
 };
